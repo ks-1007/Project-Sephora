86099a2d5464558da25e1c8a3480c132248d882b
Kumar Saurav <krsaurav1007@gmail.com>
2021-06-10 23:00:26 +0530

    added more features and improved on cart page

--- sephora/js/basket.js
+++ sephora/js/basket.js
@@ -75,6 +75,8 @@ let totalprice = 0;
 let itemcount = 0;
 let itemcountContainer = document.getElementById("itemNo");
 let totalpriceCont = document.getElementById("merchandiseTotal")
+let remainingForFreeShippingCont = document.getElementById("remainingForFreeShipping")
+let remainingForFreeShipping;
 function addToCart(item) {
     let products = JSON.parse(localStorage.getItem('productList'));
     let elem = products[item];
@@ -96,7 +98,7 @@ function addToCart(item) {
 
 let cart = document.getElementById("cart");
 let cartitems = JSON.parse(localStorage.getItem("cartList"));
-
+let estimated = document.getElementById("estimatedTotal")
 if (cartitems != null) {
     for (key in cartitems) {
         let parentdiv = document.createElement("div")
@@ -105,10 +107,15 @@ if (cartitems != null) {
         let desc = document.createElement("p")
         let quantity = document.createElement("p")
         let price = document.createElement("h4")
+        let randomInfo = document.createElement("p")
         image.src = cartitems[key].image;
         brand.innerHTML = cartitems[key].brand;
         desc.innerHTML = cartitems[key].desc;
         price.innerHTML = `$${cartitems[key].price}`
+        randomInfo.innerHTML = `
+        <p>SIZE 0.28 oz/ 8 g • ITEM 2465821</p>
+        <p>Color: 03 Tan Light</p>
+        `
         totalprice += cartitems[key].price * cartitems[key].quantity;
         itemcount += cartitems[key].quantity;
         quantity.innerHTML = cartitems[key].quantity;
@@ -119,16 +126,21 @@ if (cartitems != null) {
         let removebutton = document.createElement("button");
         removebutton.innerHTML = `<button onclick="removeFromCart('${key}')">REMOVE</button>`
         imagediv.append(image);
-        infodiv.append(brand, desc)
+        infodiv.append(brand, desc, randomInfo)
         quantitydiv.append(quantity)
         pricediv.append(price, removebutton);
         parentdiv.append(imagediv, infodiv, quantitydiv, pricediv);
         cart.append(parentdiv);
-        totalpriceCont.innerHTML = `$${totalprice}`;
+        totalpriceCont.innerHTML = `<h4>$${totalprice}</h4>`;
+        estimated.innerHTML = `<h4>$${totalprice}</h4>`;
         itemcountContainer.innerHTML = `Items in basket (${itemcount})`
     }
 }
-
+if (totalprice < 50) {
+    remainingForFreeShippingCont.innerHTML = `You're only $${50-totalprice}.00 away from Free Shipping.`
+} else {
+    remainingForFreeShippingCont.innerHTML = "You now qualify for Free Shipping!"
+}
 function removeFromCart(item) {
     // totalprice -= cartitems[item].price;
     // totalpriceCont.innerHTML = `$${totalprice}`;
