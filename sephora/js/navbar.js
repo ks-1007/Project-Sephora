4de67ceab78b80bd57613a3e5371bd3564f3a3d6
Kumar Saurav <krsaurav1007@gmail.com>
2021-06-10 13:26:04 +0530

    added one modal in navbar.html

---
+++ sephora/js/navbar.js
@@ -0,0 +1,29 @@
+let categoryNew = document.getElementById("categoryNew")
+categoryNew.addEventListener("mouseover", function () {
+    let elem1 = document.getElementById("modalContNew")
+    elem1.style.visibility = "visible"
+    // let elem2 = document.getElementById("modalNew")
+    // elem2.style.visibility = "visible"
+})
+categoryNew.addEventListener("mouseout", function () {
+    let elem1 = document.getElementById("modalContNew")
+    elem1.style.visibility = "hidden"
+    // let elem2 = document.getElementById("modalNew")
+    // elem2.style.visibility = "hidden"
+})
+
+let modalCont = document.getElementById("modalContNew")
+modalCont.addEventListener("mouseover", function() {
+    let elem1 = document.getElementById("modalContNew")
+    elem1.style.visibility = "visible"
+    // let elem2 = document.getElementById("modalNew")
+    // elem2.style.visibility = "visible"
+})
+modalCont.addEventListener("mouseout", function() {
+    let elem1 = document.getElementById("modalContNew")
+    elem1.style.visibility = "hidden"
+    // let elem2 = document.getElementById("modalNew")
+    // elem2.style.visibility = "hidden"
+})
+
+let modal = document.getElementsByClassName("")
