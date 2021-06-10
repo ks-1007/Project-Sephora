508e5eeaa12964133ec92c31b5c377786ced91a4
Kumar Saurav <krsaurav1007@gmail.com>
2021-06-10 17:00:13 +0530

    added modal on black category bar

--- sephora/js/navbar.js
+++ sephora/js/navbar.js
@@ -1,29 +1,96 @@
-let categoryNew = document.getElementById("categoryNew")
-categoryNew.addEventListener("mouseover", function () {
+// hover on New
+let categoryNew = document.getElementsByClassName("categoryNew")
+for (let i = 0; i < categoryNew.length; i++){
+    categoryNew[i].addEventListener("mouseover", function () {
     let elem1 = document.getElementById("modalContNew")
     elem1.style.visibility = "visible"
     // let elem2 = document.getElementById("modalNew")
     // elem2.style.visibility = "visible"
 })
-categoryNew.addEventListener("mouseout", function () {
+categoryNew[i].addEventListener("mouseout", function () {
     let elem1 = document.getElementById("modalContNew")
     elem1.style.visibility = "hidden"
     // let elem2 = document.getElementById("modalNew")
     // elem2.style.visibility = "hidden"
 })
+}
 
-let modalCont = document.getElementById("modalContNew")
-modalCont.addEventListener("mouseover", function() {
+
+let modalContNew = document.getElementById("modalContNew")
+modalContNew.addEventListener("mouseover", function() {
     let elem1 = document.getElementById("modalContNew")
     elem1.style.visibility = "visible"
     // let elem2 = document.getElementById("modalNew")
     // elem2.style.visibility = "visible"
 })
-modalCont.addEventListener("mouseout", function() {
+modalContNew.addEventListener("mouseout", function() {
     let elem1 = document.getElementById("modalContNew")
     elem1.style.visibility = "hidden"
     // let elem2 = document.getElementById("modalNew")
     // elem2.style.visibility = "hidden"
 })
+/////////////////////////////////////////////////////////
+// hover on Brand
+let categoryBrand = document.getElementsByClassName("categoryBrands")
+for (let i = 0; i < categoryBrand.length; i++){
+    categoryBrand[i].addEventListener("mouseover", function () {
+    let elem1 = document.getElementById("modalContBrand")
+    elem1.style.visibility = "visible"
+    // let elem2 = document.getElementById("modalNew")
+    // elem2.style.visibility = "visible"
+})
+categoryBrand[i].addEventListener("mouseout", function () {
+    let elem1 = document.getElementById("modalContBrand")
+    elem1.style.visibility = "hidden"
+    // let elem2 = document.getElementById("modalNew")
+    // elem2.style.visibility = "hidden"
+})
+}
 
-let modal = document.getElementsByClassName("")
+
+let modalContBrand = document.getElementById("modalContBrand")
+modalContBrand.addEventListener("mouseover", function() {
+    let elem1 = document.getElementById("modalContBrand")
+    elem1.style.visibility = "visible"
+    // let elem2 = document.getElementById("modalNew")
+    // elem2.style.visibility = "visible"
+})
+modalContBrand.addEventListener("mouseout", function() {
+    let elem1 = document.getElementById("modalContBrand")
+    elem1.style.visibility = "hidden"
+    // let elem2 = document.getElementById("modalNew")
+    // elem2.style.visibility = "hidden"
+})
+///////////////////////////////////////////////////////////////////////////
+// hover on MiniSize
+let categoryMiniSize = document.getElementsByClassName("categoryMiniSize")
+for (let i = 0; i < categoryMiniSize.length; i++){
+    categoryMiniSize[i].addEventListener("mouseover", function () {
+    let elem1 = document.getElementById("modalContMiniSize")
+    elem1.style.visibility = "visible"
+    // let elem2 = document.getElementById("modalNew")
+    // elem2.style.visibility = "visible"
+})
+categoryMiniSize[i].addEventListener("mouseout", function () {
+    let elem1 = document.getElementById("modalContMiniSize")
+    elem1.style.visibility = "hidden"
+    // let elem2 = document.getElementById("modalNew")
+    // elem2.style.visibility = "hidden"
+})
+}
+
+
+let modalContMiniSize = document.getElementById("modalContMiniSize")
+modalContMiniSize.addEventListener("mouseover", function() {
+    let elem1 = document.getElementById("modalContMiniSize")
+    elem1.style.visibility = "visible"
+    // let elem2 = document.getElementById("modalNew")
+    // elem2.style.visibility = "visible"
+})
+modalContMiniSize.addEventListener("mouseout", function() {
+    let elem1 = document.getElementById("modalContMiniSize")
+    elem1.style.visibility = "hidden"
+    // let elem2 = document.getElementById("modalNew")
+    // elem2.style.visibility = "hidden"
+})
+///////////////////////////////////////////////////////////////////////////
